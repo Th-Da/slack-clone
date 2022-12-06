a1ad04c5366f91eebc6c0fb14679de3082fd126c
Lukas Volgger <lukas.volgger@live.at>
2022-12-06 11:30:59 +0100

    customize file path with uid

--- src/app/_services/firestorage.service.ts
+++ src/app/_services/firestorage.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { finalize } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestorageService {
-  downloadURL: any;
+  downloadURL: Observable<string>;
 
   constructor(
     private storage: AngularFireStorage,
@@ -19,16 +19,13 @@ export class FirestorageService {
    */
   uploadImage(event: any) {
     const file = event.target.files[0];
-    const filePath = this.authService.userData.uid + '-' + 'profile-picture';
+    const filePath = this.authService.userData.uid + '_' + 'profile-picture';
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, file);
 
     // get notified when the download URL is available
     task.snapshotChanges().pipe(
-      finalize(() => {
-        this.downloadURL = fileRef.getDownloadURL();
-        console.warn('Download URL: ', this.downloadURL);
-      })
+      finalize(() => this.downloadURL = fileRef.getDownloadURL())
     ).subscribe()
   }
 }
