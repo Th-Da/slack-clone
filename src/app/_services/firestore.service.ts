c3b91ce2b0af419d7cb3aa2e9fc8b6ce3e092f24
daniel-johannsen <daniel-johannsen@web.de>
2022-12-15 23:24:14 +0100

    research bug

--- src/app/_services/firestore.service.ts
+++ src/app/_services/firestore.service.ts
@@ -138,6 +138,8 @@ export class FirestoreService {
         .subscribe((changes: any) => {
           this.directmessage = changes;
         });
+    } else {
+      console.log('no directmessagesId on upadteDirectmessage()!');
     }
   }
 
