a906141ad165b6b9c98ff1853413f4f3fbf7b24c
Lukas Volgger <lukas.volgger@live.at>
2022-12-22 07:39:07 +0100

    validate direct messages input

--- src/app/direct-message/direct-message.component.ts
+++ src/app/direct-message/direct-message.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
@@ -10,16 +11,23 @@ import { FirestoreService } from '../_services/firestore.service';
   styleUrls: ['./direct-message.component.scss'],
 })
 export class DirectMessageComponent implements OnInit {
+  directMessageForm: FormGroup;
+  @ViewChild('messageInput') messageInput;
+
   constructor(
     public authService: AuthService,
     private route: ActivatedRoute,
     public dialogRef: MatDialog,
-    public firestoreService: FirestoreService
+    public firestoreService: FirestoreService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.setDmChatId();
     this.firestoreService.updateDirectChat();
+    this.directMessageForm = this.fb.group({
+      directMessage: ['', [Validators.minLength(1)]],
+    });
   }
 
   /**
@@ -30,5 +38,17 @@ export class DirectMessageComponent implements OnInit {
       this.firestoreService.dmId = paramMap.get('uid');
       this.firestoreService.participantUid = paramMap.get('uid').split('-')[1];
     });
+  }
+
+  /**
+   * Submits the direct message form
+   */
+  onSubmit() {
+    debugger;
+    if (this.directMessageForm.valid) {
+      this.firestoreService.dmInput = this.directMessageForm.value.directMessage;
+      this.firestoreService.postDirectMessage();
+      this.messageInput.nativeElement.value = '';
+    }
   }
 }
