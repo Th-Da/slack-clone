f1509a4e39f63e3b0edf459b14d1369fb97a51fd
Lukas Volgger <lukas.volgger@live.at>
2022-12-28 16:21:34 +0100

    move img to images and more renaming

--- src/app/models/directmessage.class.ts
+++ src/app/models/directmessage.class.ts
@@ -7,7 +7,7 @@ export class Directmessage {
   constructor(obj?: any) {
     this.uid = obj ? obj.uid : '';
     this.displayName = obj ? obj.displayName : 'Guest';
-    this.photoURL = obj ? obj.photoURL : './../../../assets/img/blank_user.svg';
+    this.photoURL = obj ? obj.photoURL : './../../../assets/images/blank_user.svg';
     this.message = obj ? obj.message : '';
   }
 
