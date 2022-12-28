207b65265ce34a6c07b6283ce98a21ef3fbf7e05
Lukas Volgger <lukas.volgger@live.at>
2022-12-28 09:36:28 +0100

    implement responsive methods to chat component

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDrawer } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { DialogAccountComponent } from '../dialog-account/dialog-account.component';
 import { DialogAddChannelComponent } from '../dialog-add-channel/dialog-add-channel.component';
@@ -14,7 +15,15 @@ import { FirestoreService } from '../_services/firestore.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit {
+  @ViewChild('drawer') drawer!: MatDrawer;
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.applyResponsiveNav();
+  };
+
+  mobileNav: boolean = false;
   constructor(
     public authService: AuthService,
     public firestoreService: FirestoreService,
@@ -26,6 +35,35 @@ export class ChatComponent implements OnInit {
     this.firestoreService.getAllOtherUsers();
     this.firestoreService.getAllChannels();
     this.firestoreService.getDirectMessages();
+  }
+
+  ngAfterViewInit(): void {
+    this.applyResponsiveNav();
+  }
+
+  /**
+    * Toggle the MatDrawer in Mobile View
+    */
+  toggleNav() {
+    if (this.mobileNav) {
+      this.drawer.toggle();
+      this.drawer.mode = 'over';
+    }
+  }
+
+  /**
+   * Checks the window.innerheight and enables/disables the responsive view accordingly 
+   */
+  applyResponsiveNav() {
+    if (window.innerWidth < 768) {
+      this.drawer.close();
+      this.mobileNav = true;
+      this.drawer.mode = 'over';
+    } else {
+      this.drawer.open();
+      this.mobileNav = false;
+      this.drawer.mode = 'side';
+    }
   }
 
   openChat(url, id) {
