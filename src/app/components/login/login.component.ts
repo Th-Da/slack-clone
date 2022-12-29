68f3eb7550d2d545b2adb47c423db44485b1317c
Lukas Volgger <lukas.volgger@live.at>
2022-12-29 08:39:39 +0100

    refactor login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
-
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, Validators, } from '@angular/forms';
 import { DialogForgotPasswordComponent } from '../dialog-forgot-password/dialog-forgot-password.component';
 import { DialogGuestUserComponent } from '../dialog-guest-user/dialog-guest-user.component';
 
@@ -17,26 +11,28 @@ import { DialogGuestUserComponent } from '../dialog-guest-user/dialog-guest-user
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
   constructor(
     public authService: AuthService,
     public dialog: MatDialog
-  ) { }
-
-  contactForm = new FormGroup({
-    password: new FormControl('', Validators.required),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
-    ]),
-  });
+  ) {
+    this.loginForm = new FormGroup({
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
+    });
+  }
 
   /**
    * checks if the form is valid befor signing in
-   *  @param userEmail value from intputfield email
+   * @param userEmail value from intputfield email
    * @param userPassword value from intputfield password
    */
-  onSubmit(userEmail, userPassword) {
-    if (this.contactForm.valid) {
+  onSubmit(userEmail: string, userPassword: string) {
+    if (this.loginForm.valid) {
       this.authService.signIn(userEmail, userPassword);
     }
   }
